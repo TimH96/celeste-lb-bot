ba2aafd7d419d7817727ea008e93d6ca64f7ccca
TimHasert <timhasert@gmail.com>
2021-05-26 18:43:42 +0200

    implemented config file and CLI argument for it

--- __main__.py
+++ __main__.py
@@ -4,71 +4,21 @@ __main__.py
 Reads out settings and CLI params and runs bot with them
 """
 
-from celeste_bot import CelesteLeaderboardBot
+import sys
+import json
+from json.decoder import JSONDecodeError
+from celeste_bot  import CelesteLeaderboardBot
 
-# TODO read out settings
-config : dict = {
-    "key"   : None,
-    "timer" : 10,
-    "games" : [
-        {
-            "id"      : "o1y9j9v6",
-            "name"    : "Celeste",
-            "version" : {
-                "id_var" : "38do9y4l",
-                "id_val" : "5q8e7y3q"
-            }
-        }
-        #
-        #{
-        #    "id"      : "j1ne9me1",
-        #    "name"    : "Celeste Category Extensions",
-        #    "version" : {
-        #        "id_var" : "TODO",
-        #        "id_val" : "TODO"
-        #    }
-        #},
-        #{
-        #    "id"      : "w6j7lx46",
-        #    "name"    : "Celeste D-Sides",
-        #    "version" : {
-        #        "id_var" : "TODO",
-        #        "id_val" : "TODO"
-        #    }
-        #},
-        #{
-        #    "id"      : "w6jl3ked",
-        #    "name"    : "Celeste Custom Maps",
-        #    "version" : {
-        #        "id_var" : "TODO",
-        #        "id_val" : "TODO"
-        #    }
-        #},
-        #{
-        #    "id"      : "y6554g36",
-        #    "name"    : "Celeste Glyph",
-        #    "version" : {
-        #        "id_var" : "TODO",
-        #        "id_val" : "TODO"
-        #    }
-        #},
-        #{
-        #    "id"      : "46w3p271",
-        #    "name"    : "Celeste Quickie Mountain 2",
-        #    "version" : {
-        #        "id_var" : "TODO",
-        #        "id_val" : "TODO"
-        #    }
-        #},
-        #{
-        #    "id"      : "k6qw4q06",
-        #    "name"    : "Celeste 2020 Spring Collab",
-        #    "version" : {
-        #        "id_var" : "TODO",
-        #        "id_val" : "TODO"
-        #    }
-        #}
-    ]
-}
+# define config global
+config : dict
 
+# read out config
+try:
+    with open(sys.argv[1]) as file:
+        config = json.loads(file.read())
+except IndexError:
+    with open("./config.json") as file:
+        config = json.loads(file.read())
+
+# create and start bot
 CelesteLeaderboardBot(**config).start()
